deeac889c9300151936b4ad179ba40d21ffee43e
Kiseri  Habte <109975015+Kiserry21@users.noreply.github.com>
2023-01-16 21:11:52 -0500

    created about page

---
+++ src/components/About/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+function About() {
+    return (
+        <section>
+            <div className="center" id="about">
+				<h1 className="page-header">About Me</h1>
+			</div>
+            <div className="center">
+				<img
+					src={ProfilePic}
+					alt="about-me"
+					className="photo"
+				/>
+			</div>
+            <div className="about-me">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                
+            </div>
+        </section>
+    );
+}
+
+export default About;
