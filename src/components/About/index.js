59ff43889543962c12aa816b05cfd2821e1550a7
Kiseri  Habte <109975015+Kiserry21@users.noreply.github.com>
2023-03-14 21:25:31 -0400

    added new image

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,54 +1,45 @@
-import React from 'react';
-import AboutPic from '../../assets/images/about.JPG';
-import { Button, DatePicker, Space, version } from 'antd';
+import React from "react";
+import AboutPic from "../../assets/images/about.JPG";
+import { Button, DatePicker, Space, version } from "antd";
 import "antd/dist/reset.css";
 
-
 function About() {
-    return (
-        <section>
-            <div className="center" id="about">
-				<h1 className="page-header">About Me</h1>
-			</div>
-            <div className="center">
-				<img
-					src={AboutPic}
-					alt="about-me"
-					className="photo"
-				/>
-			</div>
-            <div className="about-me">
-                <p>
-                Hello, I'm Kiseri Habte, a recent graduate of the Rutgers University Bootcamp. 
-                My education in web development has equipped me with the skills and 
-                knowledge necessary to excel in the tech industry. 
-                I specialize in building web applications with a focus on user experience.
-                </p>
-                
-                <p>
+  return (
+    <section>
+      <div className="center" id="about">
+        <h1 className="page-header">About Me</h1>
+      </div>
+      <div className="center">
+        <img src={AboutPic} alt="about-me" className="photo" />
+      </div>
+      <div className="about-me">
+        <p>
+          Hello, I'm Kiseri Habte, a recent graduate of the Rutgers University
+          Bootcamp. My education in web development has equipped me with the
+          skills and knowledge necessary to excel in the tech industry. I
+          specialize in building web applications with a focus on user
+          experience.
+        </p>
 
-                  My background in web development combined with my passion for fitness and reading has led me to focus on 
-                  building applications that support and promote healthy lifestyles. 
-                  I am well-versed in various web development technologies, 
-                  including JavaScript, HTML, CSS, React, and more. I am always eager to learn and stay current with new technologies.
+        <p>
+          My background in web development combined with my passion for fitness
+          and reading has led me to focus on building applications that support
+          and promote healthy lifestyles. I am well-versed in various web
+          development technologies, including JavaScript, HTML, CSS, React, and
+          more. I am always eager to learn and stay current with new
+          technologies.
+        </p>
 
-                  </p>
-
-                  <p>
-
-                  Aside from coding, I enjoy staying active and keeping fit.
-                 I am a strong believer in the connection between physical and mental well-being, 
-                and I make it a priority to maintain a healthy lifestyle. 
-                I am excited to continue my journey in web development and use 
-                my skills to build applications that improve people's lives.
-
-                </p>
-               
-                
-            </div>
-            
-        </section>
-    );
+        <p>
+          Aside from coding, I enjoy staying active and keeping fit. I am a
+          strong believer in the connection between physical and mental
+          well-being, and I make it a priority to maintain a healthy lifestyle.
+          I am excited to continue my journey in web development and use my
+          skills to build applications that improve people's lives.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
